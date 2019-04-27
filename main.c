0b185f12d062b642906aa33f10b87ab37d8c3b62
David Rivas <d.rivas95@hotmail.com>
2019-04-27 13:12:25 -0500

    My first Hello World in C

--- main.c
+++ main.c
@@ -2,15 +2,7 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	int num1 = 2,
-	 num2 = 2,
-	 sum = getSum(num1, num2);
-	
-	printf("%d + %d = %d", num1, num2, sum);
+	printf("Hello world\n");
 	
 	return 0;
-}
-
-int getSum(int num1, int num2) {
-	return num1 + num2;
 }
