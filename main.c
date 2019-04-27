3a62b7c4dc4ba36bc9264b5d14ab0fce85e382c6
David Rivas <d.rivas95@hotmail.com>
2019-04-27 13:07:25 -0500

    Sum with function

--- main.c
+++ main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-
 int main(int argc, char *argv[]) {
+	int num1 = 2,
+	 num2 = 2,
+	 sum = getSum(num1, num2);
+	
+	printf("%d + %d = %d", num1, num2, sum);
+	
 	return 0;
+}
+
+int getSum(int num1, int num2) {
+	return num1 + num2;
 }
