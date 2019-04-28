c240870cc0904979fb4b49c4ccab5fd81481e22b
David Rivas <d.rivas95@hotmail.com>
2019-04-27 19:18:33 -0500

    Declaring constants

--- main.c
+++ main.c
@@ -2,10 +2,7 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	printf("%f\n", pow(2, 3));
-	printf("%f\n", sqrt(36));
-	printf("%f\n", ceil(36.356));
-	printf("%f\n", floor(36.999));
+	const int FAV_NUM = 5;
 	
 	return 0;
 }
