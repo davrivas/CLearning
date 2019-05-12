15842d001b467ae170460ca113a6dc0d2311b059
David Rivas <d.rivas95@hotmail.com>
2019-05-11 21:22:05 -0500

    Switch statements

--- main.c
+++ main.c
@@ -2,33 +2,27 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	double num1;
-	double num2;
-	char op;
+	char grade = 'F';
 	
-	printf("Enter a number\n");
-	scanf("%lf", &num1);
-	
-	printf("Enter operator\n");
-	scanf(" %c", &op);
-	
-	printf("Enter a number\n");
-	scanf("%lf", &num2);
-	
-	if (op == '+') {
-		printf("%f", num1 + num2);
-	} else if (op == '-') {
-		printf("%f", num1 - num2);
-	} else if (op == '*') {
-		 printf("%f", num1 * num2);
-	} else if (op == '/') {
-		if (num2 == 0) {
-			printf("Cannot divide by 0");
-		} else {
-			printf("%f", num1 / num2);
-		}
-	} else {
-		printf("Invalid operator");
+	switch (grade) {
+		case 'A':
+			printf("You did great!");
+			break;
+		case 'B':
+			printf("You did all right");
+			break;
+		case 'C':
+			printf("You did poorly");
+			break;
+		case 'D':
+			printf("You did very bad");
+			break;
+		case 'F':
+			printf("You failed!");
+			break;
+		default:
+			printf("Invalid grade!");
+			break;
 	}
 	
 	return 0;
