dde60046e99a62c6619b3db8b4a53a31bfe0e785
David Rivas <d.rivas95@hotmail.com>
2019-05-25 17:48:50 -0500

    Dereferencing pointers

--- main.c
+++ main.c
@@ -4,12 +4,8 @@
 int main(int argc, char *argv[]) {
 	int age = 30;
 	int * pAge = &age;
-	double gpa = 3.4;
-	double * pGpa = &gpa;
-	char grade = 'A';
-	char * pGrade = &grade;
 	
-	printf("age's memory address: %p\n", &age);
+	printf("%d", *&age);
 		
 	return 0;
 }
