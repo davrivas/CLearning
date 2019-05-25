4a944e086f25e44c613d09c96aaa17cf8d2a4dab
David Rivas <d.rivas95@hotmail.com>
2019-05-25 17:16:26 -0500

    Two dimensional arrays

--- main.c
+++ main.c
@@ -2,26 +2,20 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-	int secretNumber = 5;
-	int guess;
-	int guessCount = 0;
-	int guessLimit = 3;
-	int outOfGuesses = 0;
+	int nums[3][2] = {
+		{ 1, 2 },
+		{ 3, 4 },
+		{ 5, 6 },
+	};
 	
-	while (guess != secretNumber && outOfGuesses == 0) {
-		if (guessCount < guessLimit) {
-			printf("Enter a number\n");
-			scanf("%d", &guess);
-			guessCount++;
-		} else {
-			outOfGuesses = 1;
+	int i, j;
+	
+	for (i = 0; i < 3; i++) {
+		for (j = 0; j < 2; j++) {
+			printf("%d ", nums[i][j]);
 		}
+		printf("\n");
 	}
-	
-	if (outOfGuesses == 1)
-		printf("You lose");
-	else
-		printf("You won!");
 	
 	return 0;
 }
